18b7c4a8dd613bb71894df373a49607af3e69645
Kris Sparks <kbs5280@gmail.com>
2017-02-04 12:10:13 -0700

    Exercise complete in ES5

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,7 +3,7 @@ function SpaceAge(seconds) {
 };
 
 SpaceAge.prototype.onEarth = function() {
-  return truncateFloat(3.169e-8 * this.seconds);
+  return truncateFloat(3.16877086433912e-8 * this.seconds);
 };
 
 SpaceAge.prototype.onMercury = function() {
@@ -16,6 +16,22 @@ SpaceAge.prototype.onVenus = function() {
 
 SpaceAge.prototype.onMars = function() {
   return truncateFloat(1.6846424533248636e-8  * this.seconds);
+};
+
+SpaceAge.prototype.onJupiter = function() {
+  return (2.6722065774198312e-9 * this.seconds);
+};
+
+SpaceAge.prototype.onSaturn = function() {
+  return (1.0766666666666667e-9 * this.seconds);
+};
+
+SpaceAge.prototype.onUranus = function() {
+  return (3.769325437432647e-10 * this.seconds);
+};
+
+SpaceAge.prototype.onNeptune = function() {
+  return(1.9244534000829524e-10 * this.seconds);
 };
 
 function truncateFloat(result) {
